c7bd0e9656dc87870335ce58c84a4c06f756f4e8
Logan McDonald <loganmcdona11@gmail.com>
2016-07-15 10:14:42 -0700

    add logic for longest route and most stops to stops controller

--- app/controllers/stops_controller.rb
+++ app/controllers/stops_controller.rb
@@ -1,2 +1,29 @@
 class StopsController < ApplicationController
+  before_action :load_info 
+  def index
+    @stops = Stop.all
+  end
+
+  def map
+  end
+
+  def table
+  end
+
+  private
+  def load_info
+    @route_with_most_stops = 
+      Route.joins(:stops)
+        .select("routes.*, count(stops.id) as scount")
+        .group("routes.id")
+        .order("scount DESC")
+        .first
+    @stop_with_most_routes = 
+      Stop.joins(:routes)
+        .select("stops.*, count(routes.id) as scount")
+        .group("stops.id")
+        .order("scount DESC")
+        .first
+  end
 end
+

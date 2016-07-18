1a7b89ad1c8fac056611d4bc243d48a996e5fd6c
Logan McDonald <loganmcdona11@gmail.com>
2016-07-17 17:13:16 -0700

    add map logic for routes and stops, breaks out logic into separate js files and remove unecessary coffeescript files

---
+++ app/assets/javascripts/routes.js
@@ -0,0 +1,32 @@
+window.onload = function() {
+  var routeMap = L.map('routemap').fitBounds([
+    [gon.min_lat, gon.min_lon],
+    [gon.max_lat, gon.max_lon]
+  ]);
+
+  googleLayer = new L.Google("ROADMAP");
+  routeMap.addLayer(googleLayer);
+
+  var markers = gon.coordinates;
+
+  for (var i= 0; i < markers.length; i++) {
+    var lat = markers[i][0];
+    var lon = markers[i][1];
+    var boardings = markers[i][2];
+    var alightings = markers[i][3];
+    
+    var boardCircle = L.circle([lat, lon], boardings, {
+      color: 'red',
+      fillColor: '#f03',
+      fillOpacity: 0.5
+    }).addTo(routeMap);
+    boardCircle.bindPopup("Corner of " + markers[i][4] + " and " + markers[i][5])
+    
+    var alightCirlce = L.circle([lat, lon], alightings, {
+      color: 'blue',
+      fillColor: 'blue',
+      fillOpacity: 0.5
+    }).addTo(routeMap);
+    alightCirlce.bindPopup("Corner of " + markers[i][4] + " and " + markers[i][5])
+  }
+};
---
+++ app/assets/javascripts/stops.js
@@ -0,0 +1,33 @@
+// Search functions
+app.Stops = function() {
+  this._input = $('#stops-search-txt');
+  this._initAutocomplete();
+};
+
+app.Stops.prototype = {
+  _initAutocomplete: function() {
+    this._input
+      .autocomplete({
+        source: '/stops',
+        appendTo: '#stops-search-results',
+        select: $.proxy(this._select, this)
+      })
+      .autocomplete('instance')._renderItem = $.proxy(this._render, this);
+  },
+
+  _render: function(ul, item) {
+    var markup = [
+      `<a href="/stops/${item.id}"` + '><span class="on_street">' + item.on_street + ' and </span> ',
+      '<span class="cross_street">' + item.cross_street + '</span>',
+      '<br><span class="stop_id">' + 'Stop ' + item.stop_id + '</span></a>'
+    ];
+    return $('<li>')
+      .append(markup.join(''))
+      .appendTo(ul);
+  },
+
+  _select: function(e, ui) {
+    this._input.val(ui.item.on_street + ' and ' + ui.item.cross_street);
+    return false;
+  }
+};

bc78d7d7152a70af596108dbad7c80dc0aacce31
Logan McDonald <loganmcdona11@gmail.com>
2016-07-17 17:13:56 -0700

    add resources for stops and routes maps, remove table routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   root 'stops#index'
-  get '/map', to: 'stops#map'
-  get '/table', to: 'stops#table'
+  resources :routes
+  resources :stops do
+    get :search, :on => :collection
+  end
+  patch 'routes/plot', to: 'routes#plot'
+  post 'routes/plot', to: 'routes#plot'
 end
